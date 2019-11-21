6335f9fe08481d1f90e547878f893cc6cf16b05e
Alexey Vavilin <mnogoborec@mail.ru>
2019-11-21 11:03:36 +0300

    Parse balance with BigInt

--- __tests__/_/giver.js
+++ __tests__/_/giver.js
@@ -115,7 +115,7 @@ async function check_giver() {
     }
 
     if (!(accounts[0]["balance"]) ||
-        parseInt(accounts[0]["balance"]) < giverRequestAmount) {
+        BigInt(accounts[0]["balance"]) < giverRequestAmount) {
         throw `Giver has no money. Send some grams to ${giverWalletAddressHex} (${giverWalletAddressBase64})`
     }
 
