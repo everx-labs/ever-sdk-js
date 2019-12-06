7e4240adc816627c7b87e9c927990222cf1a4d7d
AtomXY <alexvd@inbox.ru>
2019-12-06 12:59:30 +0300

    Fixes

--- __tests__/_/init-tests.js
+++ __tests__/_/init-tests.js
@@ -1,14 +1,11 @@
 // @flow
 
-import { TONClient } from "../../src/TONClient";
-import type { TONConfigData, TONContractDeployParams, TONContractDeployResult } from "../../types";
-import { ensureBinaries } from "./binaries";
-import { deploy_with_giver, get_grams_from_giver, readGiverKeys } from "./giver";
-import { assertAbstractType } from "graphql";
+import { TONClient } from '../../src/TONClient';
+import type { TONConfigData, TONContractDeployParams, TONContractDeployResult } from '../../types';
+import { ensureBinaries } from './binaries';
+import { deploy_with_giver, get_grams_from_giver, readGiverKeys } from './giver';
 
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
 const fetch = require('node-fetch');
 const WebSocket = require('websocket');
 
@@ -16,8 +13,10 @@ export const nodeSe = !!process.env.NODESE
     && process.env.NODESE.toLowerCase() !== 'false'
     && process.env.NODESE !== '0';
 
-if (!process.env.TONCLIENTJS_SERVERS) throw new Error('Servers list is not specified');
-const serversConfig = process.env.TONCLIENTJS_SERVERS.replace(/ /gi,'').split(',');
+if (!process.env.TONCLIENTJS_SERVERS) {
+    throw new Error('Servers list is not specified');
+}
+const serversConfig = process.env.TONCLIENTJS_SERVERS.replace(/ /gi, '').split(',');
 
 
 jest.setTimeout(200_000);
@@ -25,19 +24,19 @@ jest.setTimeout(200_000);
 async function init() {
     await ensureBinaries();
 
-    //$FlowFixMe
+    // $FlowFixMe
     const library = require('../tonclient.node');
     TONClient.setLibrary({
         fetch,
         WebSocket: WebSocket.w3cwebsocket,
         createLibrary: () => {
             return Promise.resolve(library);
-        }
+        },
     });
     const client: TONClient = await TONClient.create(tests.config);
     tests.client = client;
     console.log('[Init] Created client is connected to: ', client.config.data && client.config.data.servers);
-    //await generateGiverKeys(tests.client);
+    // await generateGiverKeys(tests.client);
     await readGiverKeys();
 }
 
@@ -63,5 +62,5 @@ export const tests: {
     init,
     done,
     get_grams_from_giver,
-    deploy_with_giver
+    deploy_with_giver,
 };
