09f3e459b0f0b81f02ebf026db156a5788a1459e
Michael Vlasov <melsomino@gmail.com>
2020-12-18 11:33:08 +0500

    NEW: TypeScript declarations was added to `lib-*` packages. FIX: Some warnings related to the `entry` import was eliminated in `tests-*` packages.

---
+++ packages/lib-node/index.d.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018-2020 TON DEV SOLUTIONS LTD.
+ *
+ * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ * http://www.ton.dev/licenses
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific TON DEV software governing permissions and
+ * limitations under the License.
+ */
+
+interface BinaryLibrary {
+    setResponseHandler(
+        handler?: (
+            requestId: number,
+            paramsJson: string,
+            responseType: number,
+            finished: boolean,
+        ) => void,
+    ): void,
+
+    createContext(configJson: string): Promise<string>,
+
+    destroyContext(context: number): void,
+
+    sendRequest(
+        context: number,
+        requestId: number,
+        functionName: string,
+        functionParamsJson: string,
+    ): void,
+}
+
+export declare function libNode(): Promise<BinaryLibrary>;
