6e9acd4dcaf5cc5145068c301c275d71c36a57c6
Alexander Novikov <alex.novikov.1990@gmail.com>
2021-06-18 11:47:35 +0300

    Increase TonClient.client and TonClient.crypto test coverage, clean up crypto tests

--- packages/tests/src/tests/client.ts
+++ packages/tests/src/tests/client.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 TON DEV SOLUTIONS LTD.
+ * Copyright 2018-2021 TON DEV SOLUTIONS LTD.
  *
  * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
  * this file except in compliance with the License.
@@ -12,15 +12,29 @@
  *
  */
 
-import {
-    runner,
-} from '../runner';
+import { runner } from '../runner';
 import { test, expect } from '../jest';
 
 test('Test versions compatibility', async () => {
-    const client = runner.getClient();
-    const version = (await client.client.version()).version;
+    const client = runner.getClient().client;
+    const version = (await client.version()).version;
     console.log(version);
     expect(version.split('.')[0]).toEqual('1');
 });
 
+test('client: Versions in client.get_api_reference() and client.version() should be equal', async () => {
+    const client = runner.getClient().client;
+
+    const api_reference_version = (await client.get_api_reference()).api.version;
+    const version_version = (await client.version()).version;
+
+    expect(api_reference_version).toEqual(version_version);
+});
+
+test("client: build_info", async () => {
+    const client = runner.getClient().client;
+
+    const build_info = await client.build_info();
+
+    expect(build_info).not.toBeNull();
+})
