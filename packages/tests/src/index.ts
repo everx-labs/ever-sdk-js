9021eab31fc9023ebec26975f186f0dcacbd6dc7
Alexander Novikov <alex.novikov.1990@gmail.com>
2021-06-21 23:21:09 +0300

    [WIP] Port v0/run-local tests

--- packages/tests/src/index.ts
+++ packages/tests/src/index.ts
@@ -5,4 +5,5 @@ export * from './logger';
 export * from './tests/client';
 export * from './tests/crypto';
 export * from './tests/abi';
+export * from './tests/tvm';
 export * from './tests/net';
---
+++ packages/tests/src/tests/tvm.ts
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2018-2021 TON DEV SOLUTIONS LTD.
+ *
+ * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ * http://www.ton.dev/licenses
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific TON DEV software governing permissions and
+ * limitations under the License.
+ */
+
+import { ABIVersions, runner } from "../runner";
+import { expect, test } from "../jest";
+import { contracts } from "../contracts";
+
+import {
+    ELECTOR_ADDRESS,
+    ELECTOR_ACCOUNT,
+    ELECTOR_PARTICIPANT_LIST_AS_PLAIN_LIST,
+    ELECTOR_PARTICIPANT_LIST_AS_TUPLE_LIST,
+} from "./tvm_consts";
+
+// function base642text(base64: string): string {
+//     return Buffer.from(base64, "base64").toString("utf8");
+// }
+
+test("tvm: run_get", async () => {
+    const tvm = runner.getClient().tvm;
+
+    const tuple_participant_list = await tvm.run_get({
+        account: ELECTOR_ACCOUNT,
+        function_name: "participant_list",
+    });
+
+    expect(JSON.stringify(tuple_participant_list.output))
+        .toEqual(ELECTOR_PARTICIPANT_LIST_AS_TUPLE_LIST);
+
+    const plain_participant_list = await tvm.run_get({
+        account: ELECTOR_ACCOUNT,
+        function_name: "participant_list",
+        tuple_list_as_array: true,
+    });
+
+    expect(JSON.stringify(plain_participant_list.output))
+        .toEqual(ELECTOR_PARTICIPANT_LIST_AS_PLAIN_LIST);
+
+    const result_with_input = await tvm.run_get({
+        account: ELECTOR_ACCOUNT,
+        input: `0x${ELECTOR_ADDRESS.split(':')[1]}`,
+        function_name: "compute_returned_stake",
+    });
+
+    expect(result_with_input.output[0]).toEqual("0");
+
+    const resultPastElectionsId = await tvm.run_get({
+        account: ELECTOR_ACCOUNT,
+        function_name: "past_elections",
+    });
+
+    const pastElectionsId = resultPastElectionsId.output[0][0][0];
+    expect(pastElectionsId)
+        .toEqual("1588268660");
+});
+
+function replaceBigIntsWithNonZeroFlags(fees: { [key: string]: any }) {
+    Array.from(Object.entries(fees))
+        .forEach(([key, value]) => {
+            const s = (value || '').toString();
+            fees[key] = s !== '' && s !== '0' && s !== '0x0';
+        });
+}
+
+test.each(ABIVersions)("tvm: run_executor (ABIv%i)", async (abiVersion) => {
+    const {
+        abi,
+        net,
+        tvm,
+     } = runner.getClient();
+    
+    const walletAddress = '0:2222222222222222222222222222222222222222222222222222222222222222';
+    const subscriptionAccount = await runner.getAccount(contracts.Subscription, abiVersion, undefined, { wallet: walletAddress });
+    await runner.sendGramsTo(await subscriptionAccount.getAddress());
+    await subscriptionAccount.deploy();
+    const accountAddress = await subscriptionAccount.getAddress();
+
+    const getWalletMessage = await abi.encode_message({
+        address: accountAddress,
+        abi: subscriptionAccount.abi,
+        call_set: {
+            function_name: "getWallet",
+            input: {},
+        },
+        signer: subscriptionAccount.signer,
+    });
+
+    const getWalletResult = await tvm.run_tvm({
+        account: await subscriptionAccount.boc(),
+        message: getWalletMessage.message,
+        abi: subscriptionAccount.abi,
+    });
+
+    expect(getWalletResult.decoded?.output).toEqual({
+        value0: '0:2222222222222222222222222222222222222222222222222222222222222222',
+    });
+
+    const subscriptionParams = {
+        subscriptionId: '0x1111111111111111111111111111111111111111111111111111111111111111',
+        pubkey: '0x2222222222222222222222222222222222222222222222222222222222222222',
+        to: '0:3333333333333333333333333333333333333333333333333333333333333333',
+        value: '0x123',
+        period: '0x456',
+    };
+    
+    const subscribeMessage = await abi.encode_message({
+        address: accountAddress,
+        abi: subscriptionAccount.abi,
+        call_set: {
+            function_name: "subscribe",
+            input: subscriptionParams,
+        },
+        signer: subscriptionAccount.signer,
+    });
+
+    const subscribeResult = await tvm.run_executor({
+        account: {
+            type: "Account",
+            boc: await subscriptionAccount.boc(),
+            //unlimited_balance: true,
+        },
+        abi: subscriptionAccount.abi,
+        message: subscribeMessage.message,
+    });
+
+    replaceBigIntsWithNonZeroFlags(subscribeResult.fees);
+    expect(subscribeResult).toEqual(expect.objectContaining({
+        decoded: expect.objectContaining({
+            output: null,
+        }),
+        fees: expect.objectContaining({
+            in_msg_fwd_fee: true,
+            gas_fee: true,
+            out_msgs_fwd_fee: false,
+            total_account_fees: true,
+            total_output: false,
+        }),
+        transaction: expect.objectContaining({
+            account_addr: accountAddress,
+        }),
+    }));
+    
+    const getSubscriptionMessage = await abi.encode_message({
+        address: accountAddress,
+        abi: subscriptionAccount.abi,
+        call_set: {
+            function_name: "getSubscription",
+            input: {
+                subscriptionId: subscriptionParams.subscriptionId,
+            },
+        },
+        signer: subscriptionAccount.signer,
+    });
+
+    const getSubscriptionResult = await tvm.run_tvm({
+        account: await subscriptionAccount.boc(),
+        abi: subscriptionAccount.abi,
+        message: getSubscriptionMessage.message,
+    });
+
+    // !!! the next assertion fails with '0x00..00' pubkey instead of '0x22..22'
+    // expect(getSubscriptionResult.decoded?.output?.value0?.pubkey)
+    //     .toEqual(subscriptionParams.pubkey);
+
+    const pubkey2 = '0x3333333333333333333333333333333333333333333333333333333333333333';
+    await processing.process_message({
+        message_encode_params: {
+                address: accountAddress,
+                abi: subscriptionAccount.abi,
+                signer: subscriptionAccount.signer,
+                call_set: {
+                    function_name: "subscribe",
+                    input: { ...subscriptionParams, pubkey: pubkey2 },
+                },
+        },
+        send_events: false,
+    });
+
+    const getSubscriptionMessage2 = await abi.encode_message({
+        address: accountAddress,
+        abi: subscriptionAccount.abi,
+        call_set: {
+            function_name: "getSubscription",
+            input: {
+                subscriptionId: subscriptionParams.subscriptionId,
+            },
+        },
+        signer: subscriptionAccount.signer,
+    });
+
+    const getSubscriptionResult2 = await tvm.run_tvm({
+        account: await subscriptionAccount.boc(),
+        abi: subscriptionAccount.abi,
+        message: getSubscriptionMessage2.message,
+    });
+
+    // !!! the next assertion fails with '0x00..00' pubkey instead of '0x22..22'
+    // expect(getSubscriptionResult2.decoded?.output?.value0?.pubkey)
+    //     .toEqual(pubkey2);
+});
