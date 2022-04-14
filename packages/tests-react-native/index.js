2ebdaf5c8234161641056fb0bf9d6d80888b8df2
Sergei Voronezhskii <sergei.v@tonlabs.io>
2022-04-14 09:45:16 +0300

    Fix react-native tests

--- packages/tests-react-native/index.js
+++ packages/tests-react-native/index.js
@@ -1,23 +1,9 @@
-/*
- * Copyright 2018-2020 TON Labs LTD.
- *
- * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
- * this file except in compliance with the License.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific TON DEV software governing permissions and
- * limitations under the License.
- *
- */
 import entry from './entry';
 import { TonClient } from '@eversdk/core';
-import {libReactNative} from '@eversdk/lib-react-native';
+import { libReactNative } from '@eversdk/lib-react-native';
 import { AppRegistry } from 'react-native';
 import App from './App';
-
 entry();
-
+import {name as appName} from './app.json';
 TonClient.useBinaryLibrary(libReactNative);
-AppRegistry.registerComponent('tests_runner', () => App);
+AppRegistry.registerComponent(appName, () => App);
