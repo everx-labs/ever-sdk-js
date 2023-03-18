85dea18f896a41913a3260f66ae64f4d64cbc0f7
Sergei Voronezhskii <sergei.v@tonlabs.io>
2023-03-18 19:43:57 +0300

    Improve test runner with capability to filter test

--- packages/tests-node/run.js
+++ packages/tests-node/run.js
@@ -1,3 +1,4 @@
+const { program } = require("commander");
 const { TonClient } = require("@eversdk/core");
 const { libNode } = require("@eversdk/lib-node");
 const {
@@ -12,14 +13,24 @@ TestsRunner.exit = process.exit;
 
 TonClient.useBinaryLibrary(libNode);
 
-(async () => {
+async function run(testNames) {
     let state = zeroRunningState;
     const logger = new TestsLogger();
     await TestsRunner.run(
         (x) => state = { ...x },
-        (...args) => {
-            logger.logOutput(args.join(" ") + "\n");
+        {
+            log: (...args) => {
+                logger.logOutput(args.join(" ") + "\n");
+            },
+            filter: testNames.length > 0 ? (testEntry) =>
+                testNames.some(testName => testEntry.name.indexOf(testName) >= 0)
+            : undefined
         },
     );
     console.log(state);
+};
+
+(async () => {
+    program.argument('[testNames...]').action(run);
+    await program.parseAsync(process.argv);
 })();
